817d404fdc1d1820c4ee708b218130f196b5eafe
Hussein Elhawary <husseinelhawary@gmail.com>
2024-05-06 22:24:05 +0300

    CRTD implemented on Backend
    Co-authored-by: Moaaz Tarek <moa234@users.noreply.github.com>

---
+++ backend/src/main/java/org/cce/backend/engine/Item.java
@@ -0,0 +1,29 @@
+package org.cce.backend.engine;
+
+import com.mongodb.lang.Nullable;
+
+public class Item {
+    public String id;
+    public String content;
+    public Item right;
+    @Nullable
+    public Item left;
+    public boolean isDeleted;
+
+    Item(String id, String content) {
+        this.id = id;
+        this.content = content;
+        this.right = null;
+        this.left = null;
+        this.isDeleted = false;
+    }
+
+    Item(String id, String content, Item right, Item left, boolean isDeleted) {
+        this.id = id;
+        this.content = content;
+        this.right = right;
+        this.left = left;
+        this.isDeleted = isDeleted;
+    }
+}
+
