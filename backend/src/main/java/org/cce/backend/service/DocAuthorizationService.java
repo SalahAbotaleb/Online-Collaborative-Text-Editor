7d02e2c42ce0e5bbba7e4ee909ef09fd08ddd448
Moaaz Tarek <moaaztarek@hotmail.com>
2024-05-13 17:17:41 +0300

    move files

--- backend/src/main/java/org/cce/backend/service/DocAuthorizationService.java
+++
@@ -1,7 +0,0 @@
-package org.cce.backend.service;
-
-public interface DocAuthorizationService {
-    boolean canAccess(String docId);
-    boolean canEdit(String docId);
-    boolean fullAccess(String docId);
-}
